b7d32f032411d50d914463f70dcdbb135258bb58
“FannyFam” <fannyfam9034@gmail.com>
2020-11-27 06:40:49 +0800

    products model productsController

--- models/products.js
+++ models/products.js
@@ -1,11 +1,47 @@
 const mongoose = require("mongoose");
 // testing
 const productSchema = new mongoose.Schema({
-    name: {
+    productname: {
         type: String,
         required: true,
         unique: true,
+        // max:200
     },
+    // product_slug: {
+    //     type: String,
+    //     required: true,
+    //     unique: true
+    // },
+    imageurl: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    showfood: [{
+        foodexpiry: String,
+        foodchilled: String,
+        foodspecial: String,
+    }],
+    showcollectible: [{
+        collectspecial: { type: String },
+    }],
+    created_at: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
 });
 
 const ProductModel = mongoose.model("Product", productSchema);
