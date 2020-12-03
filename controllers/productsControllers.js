2db6b86c9b36f153b0bce3103cde6dad43b6715d
“FannyFam” <fannyfam9034@gmail.com>
2020-12-03 23:35:56 +0800

    product search upon submit

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -3,23 +3,85 @@ const ProductModel = require("../models/products");
 const productControllers = {
   create: (req, res) => {
     console.log(req.body)
-    ProductModel.create({
-      productname: req.body.productname,
-      imageurl: req.body.imageurl,
-      country: req.body.country,
-      foodexpiry: req.body.foodexpiry,
-      foodchilled: req.body.foodchilled,
-      foodspecial: req.body.foodspecial,
-      collectspecial: req.body.collectspecial,
+
+    ProductModel.findOne({
+      productname: req.body.productname
+
     })
-      .then((result) => {
-        res.statusCode = 201;
-        res.json(result);
+      .then(result => {
+        if (result != null) {
+
+          ProductModel.updateOne({
+            productname: result.productname
+          },
+            {
+              $addToSet: {
+                transaction: {
+                  url: req.body.url,
+                  qty: req.body.qty,
+                  price: req.body.price,
+                  message: req.body.message,
+                  receipt: req.body.receipt
+                }
+              }
+            },
+            { upsert: false })
+            .then((result) => {
+              res.statusCode = 201;
+              res.json(result);
+            })
+            .catch((err) => {
+              console.log(err);
+              res.send("nope");
+            });
+        }
+        else if (result == null) {
+
+          ProductModel.create({
+            productname: req.body.productname,
+            imageurl: req.body.imageurl,
+            country: req.body.country,
+            foodexpiry: req.body.foodexpiry,
+            foodchilled: req.body.foodchilled,
+            foodspecial: req.body.foodspecial,
+            collectspecial: req.body.collectspecial,
+            transaction: {
+              url: req.body.url,
+              qty: req.body.qty,
+              price: req.body.price,
+              message: req.body.message,
+              receipt: req.body.receipt
+            }
+          })
+            .then((result) => {
+              res.statusCode = 201;
+              res.json(result);
+            })
+            .catch((err) => {
+              console.log(err);
+              res.send("nope");
+            });
+        }
       })
-      .catch((err) => {
-        console.log(err);
-        res.send("nope");
-      });
+
+
+    // ProductModel.create({
+    //     productname: req.body.productname,
+    //     imageurl: req.body.imageurl,
+    //     country: req.body.country,
+    //     foodexpiry: req.body.foodexpiry,
+    //     foodchilled: req.body.foodchilled,
+    //     foodspecial: req.body.foodspecial,
+    //     collectspecial: req.body.collectspecial,
+    //   })
+    //       .then((result) => {
+    //         res.statusCode = 201;
+    //         res.json(result);
+    //       })
+    //       .catch((err) => {
+    //         console.log(err);
+    //         res.send("nope");
+    //       });
   },
   index: (req, res) => { },
 };
